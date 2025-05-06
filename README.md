7091034da924033868002a5b9b50acb1940d750b
hemukka <henri.specops@hotmail.com>
2025-05-06 06:44:28 +0300

    accept input and output files as arguments
    use argparse to accept input and output files as command line arguments, also separate loading and writing to separate functions

--- README.md
+++ README.md
@@ -2,6 +2,24 @@
 
 Convert Google Maps Timeline location history data to a more usable format and display the data on a map.
 
+# Usage
+
+In terminal or command promt, navigate to the project folder and run the script:
+
+```bash
+python src/main.py /path/to/your/Timeline.json -o /path/to/your/output.csv
+```
+
+All arguments are optional. By default:
+- input file is `Timeline.json`
+- output file is `timeline.csv` 
+
+Get full help message with `-h` or `--help` flag:
+
+```bash
+python src/main.py -h
+```
+
 
 # Roadmap
 
--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 import json
 import csv
+import argparse
 
-file_path = "../Timeline.json"
-
-def main():
-    
+def load_location_data(file_path):
+    """Load and return the data from a Google Timeline JSON file."""
     with open(file_path) as file:
         # deserialize json file to a python object
         data = json.load(file)
@@ -17,6 +16,7 @@ def main():
     locations = []
 
     for item in data['semanticSegments']:
+        # skip other segments (activity, visit, timelineMemory) 
         if "timelinePath" not in item:
             continue
         
@@ -33,10 +33,36 @@ def main():
     print(f"Number of location data points: {len(locations)}")
     print(f"First data point:\n{locations[0]}")
 
-    with open('../timeline.csv', 'w', newline='') as csvfile:
+    return locations
+
+
+def write_to_csv(locations, file_path):
+    """Write location data to CSV."""
+    if not file_path.endswith(".csv"):
+        file_path += ".csv"
+    with open(file_path, 'w', newline='') as csvfile:
         csvwriter = csv.DictWriter(csvfile, locations[0].keys())
         csvwriter.writeheader()
         csvwriter.writerows(locations)
+    print(f"Wrote location data to: {file_path}")
+
+
+def main():
+    
+    parser = argparse.ArgumentParser(description="Convert Google Timeline data to a simple CSV file.")
+    parser.add_argument("input_json",
+                        nargs="?",
+                        default="Timeline.json",
+                        help="Path to Google Timeline JSON file. Default: Timeline.json")
+    parser.add_argument("-o", "--output_csv",
+                        default="timeline.csv",
+                        help="Path to output CSV file. Overwrites existing file. Default: timeline.csv"
+                        )
+    args = parser.parse_args()
+
+    locations = load_location_data(args.input_json)
+    write_to_csv(locations, args.output_csv)
+
 
 if __name__ == "__main__":
     main()
